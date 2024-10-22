28b4bb3f0cef26cb40f68e491568c0365b3b614a
Joao Victor <joaovictornunessilva12345@gmail.com>
2024-10-22 11:25:12 -0300

    teste

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.getElementById("confirmBtn").addEventListener("click", function () {
   document.getElementById("confirmationMessage").innerText =
     "Aguardo seu retorno teimosa!";
 
-  // Mostrar a imagem da rosa
+  // Mostrar a imagem da rosa n funcionou
   const roseImage = document.getElementById("roseImage");
   roseImage.style.display = "block";
 
